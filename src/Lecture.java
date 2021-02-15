9442c345807a91aa028e13ad31451bc87f29db4d
stalkerpnv <stalker_pnv@mail.ru>
2021-02-15 15:28:37 +0900

    Initial commit

---
+++ src/Lecture.java
@@ -0,0 +1,24 @@
+
+public class Lecture {
+    String name; // null
+    int a;
+    Lecture(){
+        this.name = "default";
+        this.a = 132;
+    }
+    Lecture(String nameS, int aS){
+        name = nameS;
+        a = aS;
+    }
+    public static void teach(){
+        System.out.println("Teach");
+    }
+
+    public static void main(String[]args){
+        Lecture lecture = new Lecture();
+        //Тип  название перем = создание конструктор(метод)
+        System.out.println(lecture.name);
+    }
+    //1. 8 примитивных типов
+    //2. Объекты классов, массив
+}
